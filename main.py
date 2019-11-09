2687683b2c8aeb153fcc17aab64135ce0a91917c
XaXallle4ka <33667573+XaXallle4ka@users.noreply.github.com>
2019-11-09 15:51:02 +0300

    Git и Случайные окружности

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from PyQt5 import Qt
 from random import randint
 import sys
-from PyQt5 import uic
+from Ui import Ui_MainWindow
 
 
-class MyWidget(Qt.QMainWindow):
+class MyWidget(Qt.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui',self)
-
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.plot)
 
     def plot(self):
@@ -20,7 +19,8 @@ class MyWidget(Qt.QMainWindow):
         self.setCentralWidget(self.graphics_view)
         diameter = randint(0, 300)
         rect = Qt.QRectF(0, 0, diameter, diameter)
-        color = Qt.Qt.yellow
+        colorlist = [Qt.Qt.yellow, Qt.Qt.red, Qt.Qt.blue, Qt.Qt.green, Qt.Qt.black, Qt.Qt.gray, Qt.Qt.darkCyan]
+        color = colorlist[randint(0,6)]
 
         self.scene.addEllipse(rect, Qt.QPen(color), Qt.QBrush(color))
 
